4cf38d5468e3b4326a7f5063fcf33eafaca2fb2a
Stanislaus Dusek <dusek.s03@htlwienwest.at>
2021-09-28 10:47:49 +0200

    Removed QuickLog as it was obsolete

--- CD2_Bot/CD2_Bot/Defaults.cs
+++ CD2_Bot/CD2_Bot/Defaults.cs
@@ -10,11 +10,5 @@ namespace CD2_Bot
     public static class Defaults
     {
         public static readonly char dPrefix = '<';
-
-        //Log a string as Info in the Logging system.
-        async public static void QuickLog(string msg)
-        {
-            await Program.Log(new Discord.LogMessage(Discord.LogSeverity.Info, "Internal", msg));
-        }
     }
 }
