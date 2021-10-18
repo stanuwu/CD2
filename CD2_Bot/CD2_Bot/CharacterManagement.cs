be2a7182dde9d063d9b602eb7bfd322903ecfc38
Iftry Cus <firestrike65@gmail.com>
2021-10-18 23:39:15 +0200

    Added CharacterManagement character command

--- CD2_Bot/CD2_Bot/CharacterManagement.cs
+++ CD2_Bot/CD2_Bot/CharacterManagement.cs
@@ -17,11 +17,51 @@ namespace CD2_Bot
             {
                 await ReplyAsync("Please enter a name for your character!");
             }
+            else if(tempstorage.characters.Any(x => x.PlayerID == Context.User.Id))
+            {
+                await ReplyAsync("You already have a character!");
+            }
             else
             {
-                tempstorage.characters.Add(new CharacterStructure(charname.Substring(0, 20), Context.User.Id));
-                Utils.DebugLog(charname.Substring(0, 20));
-                await ReplyAsync($"Character {charname.Substring(0, 20)} created!");
+                if(charname.Length > 20)
+                {
+                    charname = charname.Substring(0, 20);
+                }
+                tempstorage.characters.Add(new CharacterStructure(charname, Context.User.Id));
+                await ReplyAsync($"Character {charname} created!");
+            }
+
+        }
+
+        [Command("character")]
+        [Summary("View your character.")]
+        public async Task CharacterAsync([Remainder] string xargs = null)
+        {
+
+            CharacterStructure stats = (from user in tempstorage.characters
+                                       where user.PlayerID == Context.User.Id
+                                       select user).SingleOrDefault();
+            Utils.DebugLog("Servus");
+
+            if (stats == null)
+            {
+                await ReplyAsync("You don't have a character! Create one with <start.");
+            }
+            else
+            {
+                await ReplyAsync($"Name: {stats.CharacterName} \n" +
+                    $"Title: {stats.Title} \n" +
+                    $"Description: {stats.Description} \n" +
+                    $"Class: {stats.CharacterClass} \n" +
+                    $"Money: {stats.Money} \n" +
+                    $"EXP: {stats.EXP} \n" +
+                    $"HP: {stats.HP} \n" +
+                    $"Weapon: {stats.Weapon} \n" +
+                    $"Armor: {stats.Armor} \n" +
+                    $"Extra: {stats.Extra} \n" +
+                    $"Inventory: {stats.Inventory} \n" +
+                    $"Stat Multiplier: {stats.StatMultiplier} \n" +
+                    $"Image: {Context.User.GetAvatarUrl()}");
             }
         }
     }
