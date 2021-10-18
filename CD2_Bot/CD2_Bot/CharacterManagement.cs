529ecf0c38f0f31169079b21da0c5d8bb225357a
Iftry Cus <firestrike65@gmail.com>
2021-10-18 22:30:25 +0200

    added tempstorage added charactermanagement

---
+++ CD2_Bot/CD2_Bot/CharacterManagement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace CD2_Bot
+{
+    class CharacterManagement : ModuleBase<SocketCommandContext>
+    {
+        [Command("start")]
+        [Summary("Creates a character to start the game with.")]
+        public async Task StartAsync(string charname = null, [Remainder] string xargs = null)
+        {
+            switch (string.IsNullOrWhiteSpace(charname))
+            {
+                case true:
+                    await ReplyAsync("Please enter a name for your character!");
+                    break;
+                default:
+                    tempstorage.characters.Add(new CharacterManagement())
+                    break;
+            }
+        }
+    }
+}
---
+++ CD2_Bot/CD2_Bot/tempstorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CD2_Bot
+{
+    class tempstorage
+    {
+        public static List<CharacterStructure> characters = new List<CharacterStructure> { };
+    }
+}
