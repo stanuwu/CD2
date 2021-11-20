9bef6d09747cc34de16f3c78f63adf1fca621c65
Stefan Ullrich <yoshisniper_@outlook.de>
2021-11-20 19:33:02 +0100

    Alle alten Monster (

--- CD2_Bot/CD2_Bot/Enemy.cs
+++ CD2_Bot/CD2_Bot/Enemy.cs
@@ -127,7 +127,26 @@ namespace CD2_Bot
             new Enemy("Cursed Rock", "A rock that has been cursed to roll forever, crushing everything in its path", 70, 0, 80, 0, 5, null, null, new EnemyDrops("Stone", 2, 1, 40)),
             new Enemy("Wild Hound", "A stray dog, craving only food, not affection.", 50, 0, 100, 0, 10, null, null, null),
             new Enemy("Red Lizard", "A common reptile, often mistaken for a young dragon.", 40, 0, 100, 0, 9, null, null, new EnemyDrops("Scale", 1, 0, 20)),
-            new Enemy("Goblin Bandit", "Hostile Goblins wielding melee weapons. A common, but ", 0, 0, 0, 0, 0, null, null, null)
+            new Enemy("Goblin Bandit", "Hostile Goblins wielding melee weapons.", 70, 0, 90, 5, 15, null, null, null),
+            new Enemy("Buzzing Dragonfly", "Bigger and more aggressive than normal dragonflys. Easy to kill, but their bites can cause grave wounds.", 35, 0, 150, 5, 20, null, null, null),
+            new Enemy("Skeleton Warrior", "The remains of a fallen warrior whose desire for blood hasn't even been quenched by their demise.", 45, 0, 80, 5, 25, null, null, new EnemyDrops("Rusty Sword", 1, 0, 15)),
+            new Enemy("Roaming Thundercloud", "The creation of a wizard's spell, forgotten by its caster.", 30, 0, 100, 5, 50, null, null, null),
+            new Enemy("Green Ogre", "A giant monster armed with an even larger club. Protective of its territory.", 100, 0, 75, 5, 20, null, null, null),
+            new Enemy("Stone Golem", "A giant humanoid covered in a armor of stones and minerals. Often thought to be a defense mechanism of the mountains they roam.", 100, 0, 70, 10, 20, null, null,  new EnemyDrops("Stone", 4, 1, 20)),
+            new Enemy("Toxic Smoke", "A cloud o poisonous smoke which is controlled by an otherwise weak spirit.", 60, 0, 150, 10, 10, null, null, null),
+            new Enemy("Ice Wizard of the Order", "A member of an evil organisation who specializes in ice spells.", 55, 0, 120, 10, 20, null, null, null),
+            new Enemy("Fire Wizard of the Order", "A member of an evil organisation who specializes in fire spells.", 55, 0, 100, 10, 30, null, null, null),
+            new Enemy("Grand Wizard of the Order", "An officer of an evil organisation who specializes in a variety of spells.", 70, 0, 110, 12, 25, null, null, null),
+            new Enemy("Fire Golem", "They look Like their cousin the Stone Golem, but are covered in fire and found in volcanoes rather than normal mountains", 110, 0, 80, 15, 40, null, null, null),
+            new Enemy("Wolverine Spider", "An agressive arachne, crawling in the deepest of caves. They got their name from its deadly bite bite strenght", 50, 0, 100, 15, 50, null, null, null),
+            new Enemy("Lost Spirit", "The ghost of a human who found a horrifying death, trapped in the world of the living until the kill another unfortunate soul", 50, 0, 50, 15, 40, null, null, null),
+            new Enemy("Ancient Spirit", "The ghost of a human who has forgotten their own death. Some say the ony kill in order to remember how they died", 40, 0, 50, 16, 45, null, null, null),
+            new Enemy("Hungry Vampire", "A vampire who has lost their mind through starving for too long. They may never recover again, even if they manage to drink blood.", 50, 0, 120, 15, 50, null, null, null),
+            new Enemy("Black Dragon", "A dragon who has been ousted by its family when it was young. It survived, only motiviated by its anger.", 150, 0, 70, 20, 40, null, null, new EnemyDrops("Scale", 3, 1, 10)),
+            new Enemy("Banished Reaper", "A taker of souls who enjoyed their profession for the wrong reasons.", 100, 0, 100, 20, 50, null, null, null),
+            new Enemy("Fallen Angel", "Deserters, who could not longer bear the ignorance of their maker. They were granted to roam the world of the living, but only to be punished with ill thoughts after arriving", 120, 0, 100, 20, 40, null, null, null),
+            new Enemy("Devil's Minion", "Asked and granted to bring fear into the lives of the living by the devil himsel.", 90, 0, 80, 0, 45, null, null, null),
+            new Enemy("Corrupted Spirit", "The soul of a person who killed before when they were alive. Sadly, the don't use their second chance for a ", 100, 0, 50, 25, 60, null, null, null)
         };
         }
 }
