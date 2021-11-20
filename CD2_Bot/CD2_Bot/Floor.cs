e5e81f126d7ae19fed453751f3f06d669e979c37
stanvonderhtlwienwestlol <dusek.s03@htlwienwest.at>
2021-11-20 16:55:47 +0100

    Added Room Selector Placeholder

--- CD2_Bot/CD2_Bot/Floor.cs
+++ CD2_Bot/CD2_Bot/Floor.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +31,18 @@ namespace CD2_Bot
             }
 
             stats.LastFloor = DateTime.Now;
-            await ReplyAsync(embed: Utils.QuickEmbedNormal("Success", "Floor Command Will be implemented here!"));
+
+            MessageComponent menu = new ComponentBuilder()
+                .WithSelectMenu(new SelectMenuBuilder()
+                .WithPlaceholder("Select a Room!")
+                .WithCustomId("floorselectroom;"+Context.User.Id.ToString())
+                .WithMinValues(1)
+                .WithMaxValues(1)
+                .AddOption("Test Room", "r1", "Test Text")
+                .AddOption("Test Room 2", "r2", "Text Test and more!")
+                .AddOption("Test Room 3", "r3", "Text Test and more adn even more?!")).Build();
+
+            await ReplyAsync(embed: Utils.QuickEmbedNormal("Floor", "You entered a new floor and are presented with 3 rooms. What one will you open?"), component:menu);
         }
     }
 }
