37d6c9bef17269aeda3e249f47b4aafc2cefcd6b
Iftry Cus <firestrike65@gmail.com>
2021-11-20 22:12:33 +0100

    changed damage boost for weapon and extra

--- CD2_Bot/CD2_Bot/Gear.cs
+++ CD2_Bot/CD2_Bot/Gear.cs
@@ -44,7 +44,7 @@ namespace CD2_Bot
         public string Name { get; set; }
         public string Description { get; set; }
         public int BaseDamage { get; set; }
-        public int Damage { get { return BaseDamage * (Level/50 + 1); } }
+        public int Damage { get { return BaseDamage * (Level/50 + 10); } }
         public int Level { get { return (int)Math.Floor((double)this.EXP / 250); } }
         public int EXP { get; set; }
         public string CustomEffectName { get; set; }
@@ -117,7 +117,7 @@ namespace CD2_Bot
         public string Name { get; set; }
         public string Description { get; set; }
         public int BaseDamage { get; set; }
-        public int Damage { get { return BaseDamage * (Level / 50 + 1); } }
+        public int Damage { get { return BaseDamage * (Level / 50 + 10); } }
         public int BaseHeal { get; set; }
         public int Heal { get { return BaseHeal * (Level / 500 + 1); } }
         public int Level { get { return (int)Math.Floor((double)this.EXP / 250); } }
@@ -162,9 +162,16 @@ namespace CD2_Bot
             new Extra("Scarf", "This classy piece of fabric makes you stronger purely by proxy of feeling cooler with it.", 2, 2, 0, null, null, Rarity.Common),
             new Extra("Water Flask", "Refreshes the soul just enough to let you fight a little longer.", 0, 6, 0, null, null, Rarity.Common),
             // uncommons
-            new Extra("Blade Sharpener", "Gives you that little edge.", 4, 0, 0, null, null, Rarity.Uncommon),
-            new Extra("Turbo Shroom", "Tastes horrible, but it's healthy!", 2, 3, 0, null, null, Rarity.Uncommon)
+            new Extra("Blade Sharpener", "Gives you that little edge.", 5, 0, 0, null, null, Rarity.Uncommon),
+            new Extra("Turbo Shroom", "Tastes horrible, but it's healthy!", 3, 3, 0, null, null, Rarity.Uncommon),
+            new Extra("Honeybee", "Brings you some refreshing honey every so often.", 2, 5, 0, null, null, Rarity.Uncommon),
+            new Extra("Buckler", "A small shield, only meant for quick parries.", 4, 1, 0, null, null, Rarity.Uncommon),
+            new Extra("Mirror", "Blind your enemies, giving you more time to plan your next move.", 1, 7, 0, null, null, Rarity.Uncommon),
             // rares
+            new Extra("Wand of Healing", "Allows you to access your intrinsic healing abilities.", 0, 14, 0, null, null, Rarity.Rare),
+            new Extra("Wand of Strength", "Magically boosts your fighting prowess.", 8, 0, 0, null, null, Rarity.Rare),
+            new Extra("Wand of Balance", "Every part of you feels stronger... but only slightly.", 5, 6, 0, null, null, Rarity.Rare),
+            new Extra("Black Cat", "A small familiar, helping you in every fight.", 7, 4, 0, null, null, Rarity.Rare),
 
             // epics
 
