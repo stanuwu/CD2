9fb9a9e254e2029afe4283316872ba34c2d797bc
stanvonderhtlwienwestlol <dusek.s03@htlwienwest.at>
2021-12-03 21:35:43 +0100

    fixed floor by using defer async and followup async instead of respond (should not have been necessairy but god knows what happened)

--- CD2_Bot/CD2_Bot/SelectionHandler.cs
+++ CD2_Bot/CD2_Bot/SelectionHandler.cs
@@ -37,10 +37,11 @@ namespace CD2_Bot
                 try
                 {
                     ulong gid = ((SocketGuildUser)sel.User).Guild.Id;
-                string selOpt = string.Join(", ", sel.Data.Values);
-                Tuple<Embed, Optional<MessageComponent>> results = Rooms.ExecuteRoom(selOpt, userid, gid, sel.Channel);
-                await sel.Message.DeleteAsync();
-                await sel.RespondAsync(component: results.Item2.Value, embed: results.Item1 );
+                    string selOpt = string.Join(", ", sel.Data.Values);
+                    Tuple<Embed, Optional<MessageComponent>> results = Rooms.ExecuteRoom(selOpt, userid, gid, sel.Channel);
+                    await sel.Message.DeleteAsync();
+                    await sel.DeferAsync();
+                    await sel.FollowupAsync(component: results.Item2.Value, embed: results.Item1 );
                 }
                 catch (Exception r) { Utils.DebugLog(r.Message);  }
             } else
