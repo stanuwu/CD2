b15b492ddcc5c8c4e7955e45316f982c16d29218
stanvonderhtlwienwestlol <dusek.s03@htlwienwest.at>
2022-01-23 18:36:41 +0100

    increment version

--- CD2_Bot/CD2_Bot/Defaults.cs
+++ CD2_Bot/CD2_Bot/Defaults.cs
@@ -13,7 +13,7 @@ namespace CD2_Bot
         public static readonly char dPrefix = '<';
         public static readonly List<ulong> STAFF = new List<ulong> { 159355703016947713, 255732879525412865, 623984743914012712 };
         public static DiscordSocketClient CLIENT;
-        public static readonly string VERSION = "0.4.2";
+        public static readonly string VERSION = "0.5.0";
         public static readonly string FOOTER = $"CustomDungeons 2 - Ver. {VERSION}";
         public static readonly string BOTIMG = "https://cdn.discordapp.com/avatars/887247576297013288/ab077ae898cf9ca26c3f035c5ebf15a2.webp?size=1024";
         public static readonly int FLOORCOOLDOWN = 10;
