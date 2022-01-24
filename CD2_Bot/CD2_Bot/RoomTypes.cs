4ba9bd7ee6e02fcd4e3994ee9036dea55d6b5f4e
stanvonderhtlwienwestlol <dusek.s03@htlwienwest.at>
2022-01-24 23:46:21 +0100

    fixed chest room trying to give handmade rarity price

--- CD2_Bot/CD2_Bot/RoomTypes.cs
+++ CD2_Bot/CD2_Bot/RoomTypes.cs
@@ -62,7 +62,7 @@ namespace CD2_Bot
                     embed = Utils.QuickEmbedNormal("Room", $"Lucky! You found {mfound} coins!");
                     break;
                 case "rChest":
-                    Rarity chestrarity = Prices.buy.Keys.ToList().FindAll(f => f != Rarity.Handmade)[Defaults.GRandom.Next(Prices.buy.Count)]; //this is kinda dumb but patches recieving craftables from chests
+                    Rarity chestrarity = Prices.buy.Keys.ToList().FindAll(f => f != Rarity.Handmade)[Defaults.GRandom.Next(Prices.buy.Keys.ToList().FindAll(f => f != Rarity.Handmade).Count)]; //this is kinda dumb but patches recieving craftables from chests
                     string item = "";
                     int itemchance = Defaults.GRandom.Next(0, 3);
                     switch (itemchance)
